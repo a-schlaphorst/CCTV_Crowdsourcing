1620755377185550b4bd9d5489502aff28b87332
ReneUnrau <renegolas@web.de>
2014-11-26 19:17:45 +0100

    Third draft of navigate-function
    prototype
    -route cannot be deleted
    -no preference-settings (high/low surveillance)

--- homeMap.js
+++ homeMap.js
@@ -178,42 +178,46 @@ function calculateRoute(){
 	var request = {
       origin: $('#origin-text-field').val(),
       destination: $('#destination-text-field').val(),
-      travelMode: google.maps.TravelMode["DRIVING"]
+      travelMode: google.maps.TravelMode[$("#travel-mode :radio:checked").val()]
 	};
 	directionsService.route(request, function(response, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			var path = L.Polyline.fromEncoded(response.routes[0].overview_polyline)
-			var coloredPath = getRouteSurveillance(path);
-			coloredPath.addTo(homeMap);
-			homeMap.fitBounds(coloredPath.getBounds());
+			// create array of coordinates from google-encoded path
+			var coordinates = L.PolylineUtil.decode(response.routes[0].overview_polyline)
+			
+			// array of paths to be colored
+			coloredPaths = [];
+			
+			// for each coordinates check if under surveillance
+			for(var i = 0; i < coordinates.length - 1; i++){
+				var isSurveilled = false;
+				// check if within bound of any camera
+				for(var j = 0; j < cameras.length; j++){
+					// check start and end of each path
+					if(cameras[j].getBounds().contains(coordinates[i]) || cameras[j].getBounds().contains(coordinates[i + 1])){
+						isSurveilled = true;
+						break;
+					}
+				}
+				// select red for "under surveillance" otherwise green
+				var color;
+				if(isSurveilled){
+					color = "#FF4747";
+				} else {
+					color = "#40C740";
+				}
+				
+				// create polyline
+				var polyline = L.polyline([coordinates[i],coordinates[i+1]], {color: color});
+				polyline.addTo(homeMap)
+				coloredPaths.push(polyline);
+			}		
+			// fit map-view to route
+			homeMap.fitBounds([
+				[coordinates[0][0], coordinates[0][1]],
+				[coordinates[coordinates.length-1][0], coordinates[coordinates.length-1][1]]
+			]);
 		}
 	});
-}
-
-function getRouteSurveillance(path){
-	var multiOptionsPolyline = L.multiOptionsPolyline(path, {
-		multiOptions: {
-			optionIdxFn: function (latLng) {
-				// check if segment is within surveilled area
-				for(var i = 0; i < cameras.length; i++){
-					if(cameras[i].getBounds().contains(latLng)){
-						// if true
-						return 0;
-					}
-				}			
-				// if false
-				return 1;
-			},
-			options: [
-				{color: '#FF4747'}, {color: '#40C740'}
-			]
-		},
-		weight: 5,
-		lineCap: 'butt',
-		opacity: 0.75,
-		smoothFactor: 1
-	});
-	
-	return multiOptionsPolyline;
 }
 
