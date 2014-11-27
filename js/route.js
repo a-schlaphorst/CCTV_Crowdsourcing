fdb08097e837a544042a4db688d2d6e7447f5c17
ReneUnrau <renegolas@web.de>
2014-11-27 18:25:09 +0100

    Added popup for navigate-function
    compute appr. surveilled proportion

--- js/route.js
+++ js/route.js
@@ -9,6 +9,7 @@ function calculateRoute(){
 			// create array of coordinates from google-encoded path
 			var coordinates = L.PolylineUtil.decode(response.routes[0].overview_polyline)
 			
+			var surveilledPaths = 0;
 			
 			// for each coordinates check if under surveillance
 			for(var i = 0; i < coordinates.length - 1; i++){
@@ -25,12 +26,13 @@ function calculateRoute(){
 				var color;
 				if(isSurveilled){
 					color = "#FF4747";
+					surveilledPaths++;
 				} else {
 					color = "#40C740";
 				}
 				
 				// create polyline
-				var polyline = L.polyline([coordinates[i],coordinates[i+1]], {color: color});
+				var polyline = L.polyline([coordinates[i],coordinates[i+1]], {color: color, opacity: 0.9});
 				polyline.addTo(homeMap)
 				routePaths.push(polyline);
 			}		
@@ -39,6 +41,11 @@ function calculateRoute(){
 				[coordinates[0][0], coordinates[0][1]],
 				[coordinates[coordinates.length-1][0], coordinates[coordinates.length-1][1]]
 			]);
+			
+			var surveillanceHint =  "Approx. " + Math.round(surveilledPaths / routePaths.length * 10000) / 100 + "% of this route is under surveillance!"
+			$( "#route-surveillance-text").text(surveillanceHint);
+			$( "#routeSurveillancePopup" ).popup();
+			$( "#routeSurveillancePopup" ).popup( "open" );
 			
 			// display "remove route" button
 			toggle_visibility('routeButton');
